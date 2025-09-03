4100b5f2acb4b8e30ef5f86d9e967132037384ab
Qiuerjun <2103164515@qq.com>
2025-09-03 23:04:33 +0800

    Site updated: 2025-09-03 23:04:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/11/hello-world/","2025/08/13/我的第一篇文章/"];function toRandomPost(){
+var posts=["2025/08/13/我的第一篇文章/","2025/08/11/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
