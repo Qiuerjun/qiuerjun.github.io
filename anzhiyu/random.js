b26b96d52f14b06c8e26a3181d7baa8d86fefa1f
Qiuerjun <2103164515@qq.com>
2026-03-28 15:41:30 +0800

    Site updated: 2026-03-28 15:41:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/13/我的第一篇文章/","2025/08/11/hello-world/"];function toRandomPost(){
+var posts=["2025/08/11/hello-world/","2025/09/27/“我的大学生活初期”/","2025/08/13/我的第一篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
